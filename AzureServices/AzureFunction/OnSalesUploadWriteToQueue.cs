de7e0daceb93750ad75f7c0bc89a45008f67708c
Nitish Raina <nitishraina@gmail.com>
2025-11-19 18:40:35 +0530

    function apps

---
+++ AzureServices/AzureFunction/OnSalesUploadWriteToQueue.cs
@@ -0,0 +1,29 @@
+using AzureFunction.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace AzureFunction
+{
+    public class OnSalesUploadWriteToQueue
+    {
+        private readonly ILogger<OnSalesUploadWriteToQueue> _logger;
+
+        public OnSalesUploadWriteToQueue(ILogger<OnSalesUploadWriteToQueue> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("OnSalesUploadWriteToQueue")]
+        [QueueOutput("salesDataBound", Connection = "AzureWebJobsStorage")]
+        public async Task<SalesRequest> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            string requestData = await new StreamReader(req.Body).ReadToEndAsync();
+            SalesRequest? srquest = JsonSerializer.Deserialize<SalesRequest>(requestData);
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return srquest ?? new SalesRequest();
+        }
+    }
+}
--- AzureServices/AzureFunctionProject/Controllers/HomeController.cs
+++ AzureServices/AzureFunctionProject/Controllers/HomeController.cs
@@ -1,21 +1,37 @@
 using AzureFunctionProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace AzureFunctionProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        // http://localhost:7156/api/OnSalesUploadWriteToQueue
+        [HttpPost]
+        public async Task<IActionResult> Index(SalesRequest salesRequest)
+        {
+
+            using var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("http://localhost:7156/api/");
+            using var content = new StringContent(JsonSerializer.Serialize<SalesRequest>(salesRequest));
+            var result = await client.PostAsync("OnSalesUploadWriteToQueue", content);
+            string returnValue = await result.Content.ReadAsStringAsync();
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
